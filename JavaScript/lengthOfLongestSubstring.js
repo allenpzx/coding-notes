30d45d2d0fc2c24334cd8518ccbe87ad5580c1f3
zixiu <allenpzx@icloud.com>
2020-08-07 11:02:23 +0800

    imprvoe

--- JavaScript/lengthOfLongestSubstring.js
+++ JavaScript/lengthOfLongestSubstring.js
@@ -27,6 +27,27 @@ var lengthOfLongestSubstring = function (s) {
   return max;
 };
 
+function lengthOfLongestSubstring(s) {
+  let len = 0;
+  for(let l = 0, r = 0; r < s.length; r++) {
+    const i = s.substring(l, r).indexOf(s.charAt(r));
+    if(i === -1) {
+      len = Math.max(len, r - l + 1);
+    }else {
+      l += l
+    }
+  }
+  return len
+}
+
+// 1. l对应遍历的子字符串的起始位置，r对应末尾位置
+// 2. 固定l，用r遍历
+// 3. 如果r不在map中，则把r当前遍历到的新值set到Map表中
+// 4. 如果r已经在map中，说明之前出现过，有重复的值，则需要更新字符串的起始位置l
+// 5. l更新中有两种情况：r对应的重复新值，在子串中（即在l到r区间里），l更新为map.get(s[r])+1即可；
+// r对应的重复新值，在l之前（即不在l到r区间内），l不需要更新，因为不影响本子串，重复的字符不在本子串里。
+// 总结即为：取最大 l = Math.max(map.get(s[r])+1,l)
+
 console.time("lengthOfLongestSubstring");
 const res = lengthOfLongestSubstring("abcabcbb");
 const res2 = lengthOfLongestSubstring("pwwkew");
@@ -37,10 +58,3 @@ const res6 = lengthOfLongestSubstring("aab");
 const res7 = lengthOfLongestSubstring("dvdf");
 console.log(res, res2, res3, res4, res5, res6, res7);
 console.timeEnd("lengthOfLongestSubstring");
-
-function pad(str) {
-    return Array(str.length - 4).fill('#').join('') + str.substring(str.length - 4)
-//   return str.substring(str.length - 4).padStart(str.length - 4, "#");
-}
-
-console.log(pad("123456"));
--- JavaScript/sort.js
+++ JavaScript/sort.js
@@ -117,43 +117,86 @@ console.log("result: ", shellSortResult);
 console.timeEnd("shellSort");
 console.log("                                             ");
 
+// function heapSort(arr) {
+//   function swap(arr, l, r) {
+//     const temp = arr[r];
+//     arr[r] = arr[l];
+//     arr[l] = temp;
+//   }
+//   function heaplify(arr, n, i) {
+//     let c1 = i * 2 + 1;       // left child node
+//     let c2 = i * 2 + 2;       // right child node
+//     let max = i;
+//     if (i >= n) return;
+//     if (c1 < n && arr[c1] > arr[max]) {
+//       max = c1;
+//     }
+//     if (c2 < n && arr[c2] > arr[max]) {
+//       max = c2;
+//     }
+//     if (max !== i) {
+//       swap(arr, max, i);      // make the max index on top
+//       heaplify(arr, n, max);  // deeply search in max index
+//     }
+//   }
+//   function build_heap(tree) {
+//     const lastNode = tree.length - 1;
+//     const lastNodeParent = Math.floor((lastNode - 1) / 2);
+//     for (let i = lastNodeParent; i >= 0; i--) {
+//       heaplify(tree, tree.length, i);
+//     }
+//   }
+//   function sort_heap(tree) {
+//     build_heap(tree);
+//     for (let i = tree.length - 1; i >= 0; i--) {
+//       swap(tree, 0, i);
+//       heaplify(tree, i, 0);
+//     }
+//   }
+//   sort_heap(arr);
+//   return arr;
+// }
+
 function heapSort(arr) {
   function swap(arr, l, r) {
     const temp = arr[r];
     arr[r] = arr[l];
     arr[l] = temp;
   }
-  function heaplify(arr, n, i) {
-    let c1 = i * 2 + 1;       // left child node
-    let c2 = i * 2 + 2;       // right child node
-    let max = i;
-    if (i >= n) return;
-    if (c1 < n && arr[c1] > arr[max]) {
+
+  function heaplify(arr, range, parent) {
+    const c1 = parent * 2 + 1; // left child node
+    const c2 = parent * 2 + 2; // right child node
+    let max = parent;
+    if (parent >= range) return;
+    if (c1 < range && arr[c1] > arr[max]) {
       max = c1;
     }
-    if (c2 < n && arr[c2] > arr[max]) {
+    if (c2 < range && arr[c2] > arr[max]) {
       max = c2;
     }
-    if (max !== i) {
-      swap(arr, max, i);      // make the max index on top
-      heaplify(arr, n, max);  // deeply search in max index
+    if (max !== parent) {
+      swap(arr, max, parent); // make the max index on top
+      heaplify(arr, range, max); // deeply search max index node after change the max
     }
   }
-  function build_heap(tree) {
-    const lastNode = tree.length - 1;
-    const lastNodeParent = Math.floor((lastNode - 1) / 2);
+
+  function buildHeap(tree) {
+    const lastNodeParent = Math.floor((tree.length - 1) / 2);
     for (let i = lastNodeParent; i >= 0; i--) {
       heaplify(tree, tree.length, i);
     }
   }
-  function sort_heap(tree) {
-    build_heap(tree);
+
+  function sortHeap(tree) {
+    buildHeap(tree);
     for (let i = tree.length - 1; i >= 0; i--) {
       swap(tree, 0, i);
       heaplify(tree, i, 0);
     }
   }
-  sort_heap(arr);
+
+  sortHeap(arr);
   return arr;
 }
 
@@ -161,4 +204,59 @@ console.time("heapSort");
 const heapSortResult = heapSort(list.slice());
 console.log("result: ", heapSortResult);
 console.timeEnd("heapSort");
+console.log("                                             ");
+
+function mergeSort(arr) {
+  function merge(arr, start, middle, end) {
+    // let left = arr.slice(start, middle);
+    // let right = arr.slice(middle, end + 1);
+    let left = [];
+    let right = [];
+    for (let i = start; i < middle; i++) {
+      left[i - start] = arr[i];
+    }
+    for (let i = middle; i <= end; i++) {
+      right[i - middle] = arr[i];
+    }
+    let l = 0;
+    let r = 0;
+    let i = start;
+    while (l < left.length && r < right.length) {
+      if (left[l] < right[r]) {
+        arr[i] = left[l];
+        l++;
+        i++;
+      } else {
+        arr[i] = right[r];
+        r++;
+        i++;
+      }
+    }
+    while (l < left.length) {
+      arr[i] = left[l];
+      l++;
+      i++;
+    }
+
+    while (r < right.length) {
+      arr[i] = right[r];
+      r++;
+      i++;
+    }
+  }
+  function divide(arr, l, r) {
+    if (l === r) return;
+    const m = Math.floor((l + r) / 2);
+    divide(arr, l, m);
+    divide(arr, m + 1, r);
+    merge(arr, l, m + 1, r);
+  }
+  divide(arr, 0, arr.length - 1);
+  return arr;
+}
+
+console.time("mergeSort");
+const mergeSortResult = mergeSort(list.slice());
+console.log("result: ", mergeSortResult);
+console.timeEnd("mergeSort");
 console.log("                                             ");
